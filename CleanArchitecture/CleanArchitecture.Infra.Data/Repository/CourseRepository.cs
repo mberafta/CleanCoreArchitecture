ea19dbab3a9a43ed584efd09679619bd7e206531
oui ounon <gesis@hotmail.fr>
2019-06-18 21:45:51 +0200

    Interfaces, repositories, implémentations

---
+++ CleanArchitecture/CleanArchitecture.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infra.Data.Repository
+{
+    public class CourseRepository:ICourseRepository
+    {
+        private UniversityDBContext _context;
+
+        public CourseRepository(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
